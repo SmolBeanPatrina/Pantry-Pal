0f04bdae34e815d67515008cca0d0b3d06137c01
Patrina <92826777+SmolBeanPatrina@users.noreply.github.com>
2026-01-21 15:47:05 -0800

    added front-end connection

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ const User = require('./models/User'); // Ensure this is correctly defined in yo
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://pantry-ob27hwtjv-smolbeanpatrinas-projects.vercel.app",
+  credentials: true
+}));
 const PORT = process.env.PORT || 5000;
 const mongoURI = process.env.MONGO_URI;
 const SECRET_KEY = process.env.SECRET_KEY || "defaultSecretKey"; // Fallback for SECRET_KEY
